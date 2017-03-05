9c95768e97b7e1e25cdeadd92a64db32c4c1a2d6
Silvio Melo <Silvio Melo>
2017-03-05 12:56:43 -0300

    Final da aula 05, comentada!

--- aluraframe/client/js/app/services/HttpService.js
+++ aluraframe/client/js/app/services/HttpService.js
@@ -1,57 +1,26 @@
 class HttpService {
-    
-    get(url) {
-        
-        return new Promise((resolve, reject) => {
-            
 
-            let xhr = new XMLHttpRequest();
-            
-            xhr.open('GET', url);
-            
-            xhr.onreadystatechange = () => {
-                    
-                if(xhr.readyState == 4) {
-                    
-                    if(xhr.status == 200) {   
-                        
-                        resolve(JSON.parse(xhr.responseText));  
-                    } else {
-                        
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            
-            xhr.send();
-             
-            
-        });
+    _handleErros(res) {
+        if (!res.ok) throw new Erro(res.statusText);
+        return res;
     }
-    
+
+    get(url) {
+
+        return fetch(url) // variável de escopo global.
+            .then(res => this._handleErros(res))
+            .then(res => res.json()); // pedimos  para a resposa se converter. Se fosse texto usariamos text.
+
+    }
+
     post(url, dado) {
 
+        return fetch(url, {
+            headers: { 'Content-type': 'application/json' },
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+            .then(res => this._handleErros(res));
 
-            return new Promise((resolve, reject) => {
-
-                let xhr = new XMLHttpRequest();
-                xhr.open("POST", url, true);
-                xhr.setRequestHeader("Content-type", "application/json");
-                xhr.onreadystatechange = () => {
-
-                    if (xhr.readyState == 4) {
-
-                        if (xhr.status == 200) {
-
-                            resolve(JSON.parse(xhr.responseText));
-                        } else {
-
-                            reject(xhr.responseText);
-                        }
-                    }
-                };
-                xhr.send(JSON.stringify(dado)); // usando JSON.stringifly para converter objeto em uma string no formato JSON.
-            });
-
-        }
+    }
 }

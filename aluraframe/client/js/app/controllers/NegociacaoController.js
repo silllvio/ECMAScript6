e28a052a2f26731bb6e80f0cc82aa690bf67dd37
Silvio Melo <Silvio Melo>
2017-03-04 21:47:41 -0300

    Final da aula 03, comentada!

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,79 +1,104 @@
 class NegociacaoController {
-    
+
     constructor() {
-        
+
         let $ = document.querySelector.bind(document);
-        
+
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-         
+
         this._listaNegociacoes = new Bind(
-            new ListaNegociacoes(), 
-            new NegociacoesView($('#negociacoesView')), 
-            'adiciona', 'esvazia' , 'ordena', 'inverteOrdem');
-       
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 'esvazia', 'ordena', 'inverteOrdem');
+
         this._mensagem = new Bind(
             new Mensagem(), new MensagemView($('#mensagemView')),
-            'texto');    
-            
-        this._ordemAtual = ''               
+            'texto');
+
+        this._ordemAtual = '';
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.findAll())
+            .then(negociacoes =>
+                negociacoes.forEach(negociacao =>
+                    this._listaNegociacoes.adiciona(negociacao)))
+            .catch(erro => {
+                console.log(erro);
+                this._mensagem.texto = 'Erro na busca do banco pela informações.';
+            });
     }
-    
+
     adiciona(event) {
-       
+
         event.preventDefault();
-        try {
-            this._listaNegociacoes.adiciona(this._criaNegociacao());
-            this._mensagem.texto = 'Negociação adicionada com sucesso'; 
-            this._limpaFormulario();   
-        } catch(erro) {
-            this._mensagem.texto = erro;
-        }
+
+        ConnectionFactory
+            .getConnection() // caso tenha sucesso o dao será estanciado.
+            .then(connection => {
+                let negociacao = this._criaNegociacao();
+                new NegociacaoDao(connection)
+                    .add(negociacao) // Essa parte grava no banco de dados.
+                    .then(() => { // se a conexão for aberta continua.
+                        this._listaNegociacoes.adiciona(negociacao); // Essa parte grava na tela do usuário.
+                        this._mensagem.texto = 'Negociação adicionada com sucesso';
+                        this._limpaFormulario();
+                    });
+            }).catch(erro => this._mensagem.texto = erro);
     }
-    
+
     importaNegociacoes() {
-        
+
 
         let service = new NegociacaoService();
         service
             .obterNegociacoes()
             .then(negociacoes => negociacoes.forEach(negociacao => {
                 this._listaNegociacoes.adiciona(negociacao);
-                this._mensagem.texto = 'Negociações do período importadas'   
+                this._mensagem.texto = 'Negociações do período importadas'
             }))
-            .catch(erro => this._mensagem.texto = erro);                              
+            .catch(erro => this._mensagem.texto = erro);
     }
-    
+
     apaga() {
-        
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso';
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.delete())
+            .then(mensagem => {
+                this._mensagem.texto = mensagem;
+                this._listaNegociacoes.esvazia(); // limpa o modelo
+            })
+
     }
-    
+
     _criaNegociacao() {
-        
+
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value);    
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value));
     }
-    
+
     _limpaFormulario() {
-     
+
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
-        this._inputData.focus();   
+        this._inputData.focus();
     }
-    
+
     ordena(coluna) {
-        
-        if(this._ordemAtual == coluna) {
-            this._listaNegociacoes.inverteOrdem(); 
+
+        if (this._ordemAtual == coluna) {
+            this._listaNegociacoes.inverteOrdem();
         } else {
-            this._listaNegociacoes.ordena((p, s) => p[coluna] - s[coluna]);    
+            this._listaNegociacoes.ordena((p, s) => p[coluna] - s[coluna]);
         }
-        this._ordemAtual = coluna;    
+        this._ordemAtual = coluna;
     }
 }
